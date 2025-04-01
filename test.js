ccd64a8d8c6742c706fd8e0857346caaea0f1432
justinjones38 <159663859+justinjones38@users.noreply.github.com>
2025-04-01 00:24:24 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,17 @@
 let x = 4;
 
 let y = 8;
+let z = 10; 
 
-console.log(x + y);
+console.log(x + y + z);
 
 if(x == 4) {
     console.log("Hello")
 } else {
     console.log("no ")
+}
+
+while (z < x) {
+    z += 2;
+    console.log(z);
 }
